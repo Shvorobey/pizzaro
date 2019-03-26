533f3b45768fdbd56deb9789c0375bc60326f643
Shvorobey <a.shvorobey@gmail.com>
2019-03-26 09:51:44 +0200

    Добавление обратного отсчета

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,28 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CYanone+Kaffeesatz:200,300,400,700"
           rel="stylesheet">
     <link rel="shortcut icon" href="/assets/images/fav-icon.png">
+    <link rel="stylesheet" href="/css/countdown.demo.css" type="text/css">
+
+    <link rel="stylesheet" href="/css/soon.min.css" type="text/css">
+    <style>
+        html,body{margin:0;padding:0;line-height:1.5;font-family:'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', sans-serif;}body p{text-align:center;}
+    </style>
+    <script src="/js/soon.min.js"></script>
+
+
+    <style>
+        body p{text-align:center;color:#ccc;}a{color:#eee;}
+    </style>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script>
+        window.jQuery(function ($){
+            "use strict";
+            $('time').countDown({with_separators:false});
+            $('.alt-1').countDown({css_class:'countdown-alt-1'});
+            $('.alt-2').countDown({css_class:'countdown-alt-2'});
+        });
+    </script>
 </head>
 <body id="top" class="@yield ('body-classes')">
 <div id="page" class="hfeed site">
